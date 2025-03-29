aa3a3b347354d3493fcc1462ca46f0e9033edc44
Abdulhadi <abdulhadi.aljabban@gmail.com>
2025-03-29 10:55:02 +0200

    empty eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,3 @@
-export default tseslint.config({
-  rules: {
-    // Note: you must disable the base rule as it can report incorrect errors
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-  },
-});
+const eslintConfig = [];
+
+export default eslintConfig;
