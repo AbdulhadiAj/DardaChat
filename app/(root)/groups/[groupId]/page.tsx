d493820de445d374bd2c01bfb42d171b67124cf1
Abdulhadi <abdulhadi.aljabban@gmail.com>
2025-03-29 11:15:25 +0200

    fix params for group

--- app/(root)/groups/[groupId]/page.tsx
+++ app/(root)/groups/[groupId]/page.tsx
@@ -5,20 +5,19 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import Header from "./_components/Header";
 import Body from "@/components/shared/chat/chat-page-components/body/Body";
 import ChatInput from "@/components/shared/chat/chat-page-components/input/ChatInput";
 import DeleteGroupDialog from "./_components/dialogs/DeleteGroupDialog";
 import LeaveGroupDialog from "./_components/dialogs/LeaveGroupDialog";
 
-type Props = {
-  params: {
-    groupId: Id<"chats">;
-  };
-};
+type Params = Promise<{ groupId: Id<"chats"> }>;
 
-const ChatPage = ({ params: { groupId } }: Props) => {
+const ChatPage = (props: { params: Params }) => {
+  const params = use(props.params);
+  const groupId = params.groupId;
+
   const chat = useQuery(api.chat.get, { id: groupId });
 
   const [deleteGroupDialogOpen, setDeleteGroupDialogOpen] = useState(false);
