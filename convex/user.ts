0ee89a6673ec46d08b9959b4ae3e9f7749888bbb
Abdulhadi <abdulhadi.aljabban@gmail.com>
2025-04-30 13:31:57 +0300

    fix delete user

--- convex/user.ts
+++ convex/user.ts
@@ -39,6 +39,22 @@ export const update = internalMutation({
   },
 });
 
+// export const deleteUser = internalMutation({
+//   args: {
+//     clerkId: v.string(),
+//   },
+//   handler: async (ctx, args) => {
+//     const user = await ctx.db
+//       .query("users")
+//       .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
+//       .first();
+
+//     if (user) {
+//       await ctx.db.delete(user._id);
+//     }
+//   },
+// });
+
 export const deleteUser = internalMutation({
   args: {
     clerkId: v.string(),
@@ -49,8 +65,47 @@ export const deleteUser = internalMutation({
       .filter((q) => q.eq(q.field("clerkId"), args.clerkId))
       .first();
 
-    if (user) {
-      await ctx.db.delete(user._id);
-    }
+    if (!user) return;
+
+    const friendships = await ctx.db
+      .query("friends")
+      .filter((q) =>
+        q.or(q.eq(q.field("user1"), user._id), q.eq(q.field("user2"), user._id))
+      )
+      .collect();
+
+    await Promise.all(
+      friendships.map(async (friendship) => {
+        const { chatId, _id: friendshipId } = friendship;
+
+        await ctx.db.delete(chatId);
+
+        const memberships = await ctx.db
+          .query("chatMembers")
+          .withIndex("by_chatId", (q) => q.eq("chatId", chatId))
+          .collect();
+
+        await Promise.all(
+          memberships.map(async (membership) => {
+            await ctx.db.delete(membership._id);
+          })
+        );
+
+        const messages = await ctx.db
+          .query("messages")
+          .withIndex("by_chatId", (q) => q.eq("chatId", chatId))
+          .collect();
+
+        await Promise.all(
+          messages.map(async (message) => {
+            await ctx.db.delete(message._id);
+          })
+        );
+
+        await ctx.db.delete(friendshipId);
+      })
+    );
+
+    await ctx.db.delete(user._id);
   },
 });
